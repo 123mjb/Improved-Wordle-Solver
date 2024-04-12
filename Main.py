661dfb239bdc1f8a1df2bb696ca4e54386549e54
123mjb <75265969+123mjb@users.noreply.github.com>
2024-04-12 07:25:37 +0000

    dwadsawd

--- Main.py
+++ Main.py
@@ -1,2 +1,13 @@
-
-def 
+class WordleSolver():
+    def __init__(self) -> None:
+        with open("NewWordleSolver/Answers.txt","r",encoding="utf-8") as f:
+            self.words = f.read().split("\n")
+        self.previousGuesses = {}
+        self.invalidLetters = []
+        self.yellowLetters = {}
+        self.validWordsfirst = []
+    def findValid(self):
+        self.validWordsfirst = []
+        for i, word in enumerate(self.words):
+            for letter in word:
+                if 
